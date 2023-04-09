30e984c9157e48810d55effabe600a861bb1491f
Debbie Otuagomah <thatgirldorian@gmail.com>
2023-04-09 11:48:00 +0400

    Added api page for trips

---
+++ pages/api/trips.js
@@ -0,0 +1,9 @@
+export default function handler(req, res) {
+  if (req.method !== "GET") {
+    res.status(405).json({
+      message: "Method Not Allowed",
+    });
+    return;
+  }
+  res.status(200).json({ test: "TRIPS!" });
+}
