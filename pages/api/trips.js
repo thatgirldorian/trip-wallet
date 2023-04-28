9ed51f2b3dec2ecaafc7e282431abd3cf09f523e
Debbie Otuagomah <thatgirldorian@gmail.com>
2023-04-28 20:14:04 +0400

    feat: Used Prisma to retrieve data

--- pages/api/trips.js
+++ pages/api/trips.js
@@ -1,9 +1,13 @@
-export default function handler(req, res) {
+import prisma from "../../lib/prisma";
+
+export default async function handler(req, res) {
   if (req.method !== "GET") {
-    res.status(405).json({
+    res.status(405).send({
       message: "Method Not Allowed",
     });
     return;
   }
-  res.status(200).json({ test: "TRIPS!" });
+
+  const trips = await prisma.trip.findMany();
+  res.status(200).json(trips);
 }
