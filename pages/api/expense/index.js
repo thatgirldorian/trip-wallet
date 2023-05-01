3670196582e5a813d38b0dc92ea9e80efa7c9932
Debbie Otuagomah <40691059+thatgirldorian@users.noreply.github.com>
2023-05-01 18:26:03 +0400

    feat: implemented GET expenses & POST expenses endpoints

--- pages/api/expense/index.js
+++
@@ -1,9 +0,0 @@
-import prisma from "lib/prisma";
-
-export default async function handler(req, res) {
-  if (req.method === "POST") {
-    return res.status(200).end();
-  }
-
-  res.status(405).json({ message: "Method Not Allowed" });
-}
