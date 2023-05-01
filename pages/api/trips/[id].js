630f0836f122b0a1867608b38222b56241feeb1d
Debbie Otuagomah <40691059+thatgirldorian@users.noreply.github.com>
2023-05-01 13:56:38 +0400

    feat: Added error handling to GET /trips/:id

--- pages/api/trips/[id].js
+++ pages/api/trips/[id].js
@@ -1,5 +1,20 @@
 import prisma from "../../../lib/prisma";
 
 export default async function handler(req, res) {
-  res.status(200).json({ ok: true });
+  if (req.method === "GET") {
+    const trip = await prisma.trip.findUnique({
+      where: {
+        id: parseInt(req.query.id),
+      },
+    });
+    if (!trip) {
+      return res.status(404).json({ message: "Trip ID does not exist :(" });
+    }
+    res.status(200).json(trip);
+  }
+
+  if (req.method === "PUT") {
+  }
+  if (req.method === "DELETE") {
+  }
 }
