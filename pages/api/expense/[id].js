f222672bd316be3754e53774c687058d97006860
Debbie Otuagomah <40691059+thatgirldorian@users.noreply.github.com>
2023-05-01 15:20:04 +0400

    feat: added expense endpoints

---
+++ pages/api/expense/[id].js
@@ -0,0 +1,19 @@
+import prisma from "lib/prisma";
+
+export default async function handler(req, res) {
+  if (req.method === "GET") {
+    const expense = await prisma.expense.findUnique({
+      where: {
+        id: parseInt(req.query.id),
+      },
+    });
+    if (!expense) {
+      return res.status(404).json({ message: "There is no such expense" });
+    }
+    res.status(200).json({ ...expense });
+  }
+  if (req.method === "PUT") {
+  }
+  if (req.method === "DELETE") {
+  }
+}
