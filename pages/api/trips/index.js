2e7c35f15348bcad35256ddde272ea559a9c42da
Debbie Otuagomah <40691059+thatgirldorian@users.noreply.github.com>
2023-05-01 14:47:43 +0400

    feat: implemented POST /trips endpoint

--- pages/api/trips/index.js
+++ pages/api/trips/index.js
@@ -1,13 +1,40 @@
 import prisma from "lib/prisma";
+import { ApiError } from "next/dist/server/api-utils";
 
 export default async function handler(req, res) {
-  if (req.method !== "GET") {
-    res.status(405).send({
-      message: "Method Not Allowed",
-    });
+  if (req.method === "GET") {
+    const trips = await prisma.trip.findMany();
+    res.status(200).json(trips);
     return;
   }
 
-  const trips = await prisma.trip.findMany();
-  res.status(200).json(trips);
+  if (req.method === "POST") {
+    const { user, name, start_date, end_date } = req.body;
+
+    if (!user) {
+      return res
+        .status(400)
+        .json({ message: "Missing a required parameter: `user`" });
+    }
+
+    if (!name) {
+      return res
+        .status(400)
+        .json({ message: "Missing a requird paraneter: `name`" });
+    }
+
+    await prisma.trip.create({
+      data: {
+        user,
+        name,
+        start_date,
+        end_date,
+      },
+    });
+
+    console.log(req.body);
+    res.status(200).end();
+  }
+
+  res.status(405).json({ message: "Method Not Allowed" });
 }
